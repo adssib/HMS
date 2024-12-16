ec3ef9991b6ed65492f2c26d643f0d5067e083dd
adib <adibakkari@gmail.com>
2024-12-16 01:35:29 -0500

    adding connection to local MSSQL server

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 Hotel Management System (aka HMS)
+
+DB used: MSSQL
+
+make sure to have telnet enabled on your laptop to be able to connect to it locally 
+
+also if not done open a port for SQL connection from your fire wall 
+
+SQL server browser enabled
+
+In SQL Server Configuration Manager, go to SQL Server Network Configuration > Protocols for [Your Instance Name].
+Ensure that both TCP/IP and Named Pipes are enabled.
--- server/backend/Server.go
+++ server/backend/Server.go
@@ -1,17 +1,70 @@
 package main
 
 import (
- "fmt"
- "log"
- "net/http"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/joho/godotenv"
 )
 
 func main() {
- fmt.Println("Starting Server...")
- http.HandleFunc("/", handler)
- log.Fatal(http.ListenAndServe(":8080", nil))
-}
+	// Load environment variables from the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-func handler(w http.ResponseWriter, r *http.Request) {
- fmt.Fprint(w, "Hello, World!")
+	// Get database credentials from environment variables
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbServer := os.Getenv("DB_SERVER")
+
+	// Construct the connection string
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;database=%s", dbServer, dbUser, dbPassword, dbName)
+
+	// Open a connection to the database
+	db, err := sql.Open("mssql", connString)
+	if err != nil {
+		log.Fatal("Error opening connection: ", err.Error())
+	}
+	defer db.Close()
+
+	// Ping the database to ensure the connection is successful
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Error pinging the database: ", err.Error())
+	}
+
+	fmt.Println("Successfully connected to the database!")
+
+	// Query the Guest table for the top 3 rows
+	rows, err := db.Query("SELECT TOP 3 guest_id, first_name, last_name FROM Guest")
+	if err != nil {
+		log.Fatal("Error running query: ", err.Error())
+	}
+	defer rows.Close()
+
+	// Iterate over the result set
+	for rows.Next() {
+		var guest_id int
+		var first_name, last_name string
+
+		// Scan the row values into the variables
+		err := rows.Scan(&guest_id, &first_name, &last_name)
+		if err != nil {
+			log.Fatal("Error scanning row: ", err.Error())
+		}
+
+		// Print the results
+		fmt.Printf("Guest ID: %d, First Name: %s, Last Name: %s\n", guest_id, first_name, last_name)
+	}
+
+	// Check for errors after iterating over rows
+	err = rows.Err()
+	if err != nil {
+		log.Fatal("Error iterating rows: ", err.Error())
+	}
 }
